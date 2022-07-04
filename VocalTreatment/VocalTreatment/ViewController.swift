c40f922a4c2caf78648a80fcbe8f14fdfa6c3f3b
P_SeoB <akdlsjtm21@naver.com>
2022-07-04 15:46:54 +0900

    build: add Snapkit package

--- VocalTreatment/VocalTreatment/ViewController.swift
+++ VocalTreatment/VocalTreatment/ViewController.swift
@@ -6,13 +6,12 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
     }
-
 }
 
